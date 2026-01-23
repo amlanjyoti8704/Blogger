e686ce5da0f88653576a8785b6055b14e6d24473
amlanjyoti8704 <162537690+amlanjyoti8704@users.noreply.github.com>
2026-01-24 01:36:45 +0530

    fix(cors): update CORS configuration for improved security and consistency

--- Server/server.js
+++ Server/server.js
@@ -7,23 +7,29 @@ import postRoutes from './Routes/postRoutes.js';
 import { configDotenv } from 'dotenv';
 import path from 'path';
 
+configDotenv();
 const app=express();
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://blogger-aj.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'token'],
+}));
+// 👇 THIS LINE IS CRITICAL
+app.options('*', cors());
 
-// connect to database
-await connectDB();
+
 
 // middlewares
-app.use(cors(
-    {
-        origin: '*'
-    }
-));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-configDotenv();
 
-// connect to cloudinary
+// connect to cloudinary and database
 await connectCloudinary();
+await connectDB();
 
 app.get('/', (req,res)=>{
     res.send('API is running...');
@@ -38,7 +44,6 @@ app.use('/uploads', express.static('uploads'));
 
 // PORT
 const PORT=process.env.PORT || 8000;
-
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`);
 });
